f842af733f9bb3e8b01c010e6e3de2182d8b58c5
Chad Sorensen <chdsorensen@gmail.com>
2011-02-02 12:27:25 -0800

    default rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burch::Application.config.secret_token = '72b5d65a0af24978befe44fc5deafa4510dec636a707d66698d42403ad4189a17e249c3099c95cf2e59065947237f5d2454d74352172e5488d81a754b76b53f9'
